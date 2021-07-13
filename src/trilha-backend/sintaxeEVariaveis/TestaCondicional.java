4c9d08d084d8df2e84fe2913c70d14a0e94de640
Orlando <muckyo@gmail.com>
2021-07-13 14:32:49 -0300

    Adicionando códigos da aula sobre Condicionais

--- src/trilha-backend/sintaxeEVariaveis/TestaCondicional.java
+++ src/trilha-backend/sintaxeEVariaveis/TestaCondicional.java
@@ -5,16 +5,15 @@ public class TestaCondicional {
     public static void main(String[] args) {
         System.out.println("testando Condicionais:\n");
         int idade = 17, quantidadePessoas = 2;
+        boolean acompanhado = quantidadePessoas >= 2;
 
-        if(idade >= 18) {
+        if(idade >= 18 && quantidadePessoas >= 2) {
             System.out.println("Você tem mais de 18 anos.");
             System.out.println("Seja bem vindo!");
-        }else {
-            if(quantidadePessoas >= 2) {
+        }else if(acompanhado) {
                 System.out.println("Você é menor de idade, mas está acompanhado.");
-            }else {
-                System.out.println("Você não é bem vindo.");
-            }
+        }else {
+            System.out.println("Você não é bem vindo.");
         }
 
         if(idade == 20)
