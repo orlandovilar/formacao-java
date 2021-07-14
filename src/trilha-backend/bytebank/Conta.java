69ac7f423d322340b2cc7493e5b13e46b0f12f05
Orlando <muckyo@gmail.com>
2021-07-14 16:00:11 -0300

    Adicionando códigos da aula sobre OO

--- src/trilha-backend/bytebank/Conta.java
+++ src/trilha-backend/bytebank/Conta.java
@@ -4,7 +4,7 @@ public class Conta {
     double saldo;
     int agencia;
     int numero;
-    String titular;
+    Cliente titular;
 
     public void depositar(double valor) {
         this.saldo += valor;
--- src/trilha-backend/bytebank/MovimentarConta.java
+++ src/trilha-backend/bytebank/MovimentarConta.java
@@ -3,30 +3,37 @@ package bytebank;
 public class MovimentarConta {
 
     public static void main(String[] args) {
+        Cliente paulo = new Cliente();
         Conta contaDoPaulo = new Conta();
         contaDoPaulo.saldo = 100;
-        contaDoPaulo.titular = "Paulo Roberto";
+        contaDoPaulo.titular = paulo;
+        contaDoPaulo.titular.nome = "Paulo Roberto";
+        contaDoPaulo.titular.cpf = "222.222.222-22";
+        contaDoPaulo.titular.profissao = "Técnico de TI";
 
         contaDoPaulo.depositar(50);
 
-        System.out.println(contaDoPaulo.titular + ": " + contaDoPaulo.saldo);
+        System.out.println(contaDoPaulo.titular.nome + ": " + contaDoPaulo.saldo);
 
         boolean conseguiuRetirar = contaDoPaulo.sacar(20);
-        System.out.println(contaDoPaulo.titular + ": " + contaDoPaulo.saldo);
+        System.out.println(contaDoPaulo.titular.nome + ": " + contaDoPaulo.saldo);
         System.out.println(conseguiuRetirar);
 
         conseguiuRetirar = contaDoPaulo.sacar(2000);
-        System.out.println(contaDoPaulo.titular + ": " + contaDoPaulo.saldo);
+        System.out.println(contaDoPaulo.titular.nome + ": " + contaDoPaulo.saldo);
         System.out.println(conseguiuRetirar);
 
+        Cliente marcela = new Cliente();
+        marcela.nome = "Marcela Lira";
+        marcela.cpf = "333.333.333-33";
+        marcela.profissao = "Advogada";
         Conta contaDaMarcela = new Conta();
         contaDaMarcela.depositar(1000);
-        contaDaMarcela.titular = "Marcela Lira";
-        System.out.println(contaDaMarcela.titular + ": " + contaDaMarcela.saldo);
+        System.out.println(marcela.nome + ": " + contaDaMarcela.saldo);
 
         boolean conseguiuTransferir = contaDaMarcela.transferir(300, contaDoPaulo);
-        System.out.println(contaDaMarcela.titular + ": " + contaDaMarcela.saldo);
-        System.out.println(contaDoPaulo.titular + ": " + contaDoPaulo.saldo);
+        System.out.println(marcela.nome + ": " + contaDaMarcela.saldo);
+        System.out.println(marcela.nome + ": " + contaDoPaulo.saldo);
         System.out.println(conseguiuTransferir);
     }
 }
