1c15b81b50817aa6cce38918538bf054bdfb0f9c
Orlando <muckyo@gmail.com>
2021-07-14 22:47:13 -0300

    Adicionando códigos das aulas sobre OO e Polimorfismo

---
+++ src/trilha-backend/bytebankHerdado/Gerente.java
@@ -0,0 +1,25 @@
+package bytebankHerdado;
+
+public class Gerente extends FuncionarioVersao1 {
+    private int senha;
+
+    public boolean autenticar(int senha) {
+        if(this.senha == senha) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    /*public double getBonificacao() {
+        return this.salario;
+    }*/
+
+
+
+
+}
---
+++ src/trilha-backend/bytebankHerdado/TestarGerente.java
@@ -0,0 +1,17 @@
+package bytebankHerdado;
+
+public class TestarGerente {
+    public static void main(String[] args) {
+        Gerente gerente1 = new Gerente();
+        gerente1.setNome("Renato");
+        gerente1.setCpf("123456789");
+        gerente1.setSalario(5000.0);
+        gerente1.setSenha(2222);
+
+        System.out.println(gerente1.getNome());
+        System.out.println(gerente1.getCpf());
+        System.out.println(gerente1.getSalario());
+
+        System.out.println(gerente1.autenticar(2222));
+    }
+}
