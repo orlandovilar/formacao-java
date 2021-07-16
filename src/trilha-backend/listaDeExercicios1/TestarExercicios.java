7253805a43a75a21115d8b84a8a76d8e1a41f5e4
Orlando <muckyo@gmail.com>
2021-07-16 02:04:48 -0300

    Adicionando alterações e melhorias nos códigos da questão 1 da lista de exercícios

--- src/trilha-backend/listaDeExercicios1/TestarExercicios.java
+++ src/trilha-backend/listaDeExercicios1/TestarExercicios.java
@@ -1,22 +1,18 @@
 package listaDeExercicios1;
 
 import listaDeExercicios1.exercicio01.Pessoa;
-
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
+/**
+ * @author José Orlando R. Vilar
+ */
 public class TestarExercicios {
 
     public static void main(String[] args) throws ParseException {
-        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-        Pessoa pessoa1 = new Pessoa("José Orlando", formato.parse("25/03/1993"), 1.74);
-        pessoa1.setNome("Tulio Pereira");
+        Pessoa pessoa1 = new Pessoa("José Orlando", "25/03/1993", 1.74);
         pessoa1.imprimirDados();
-        pessoa1.calcularEImprimirIdade();
 
-        Pessoa pessoa2 = new Pessoa("Gustavo Ghastine", formato.parse("06/11/1978"), 1.88);
+        Pessoa pessoa2 = new Pessoa("Gustavo Ghastine", "06/11/1978", 1.88);
         pessoa2.imprimirDados();
-        pessoa2.calcularEImprimirIdade();
-        
     }
 }
--- src/trilha-backend/listaDeExercicios1/exercicio01/Pessoa.java
+++ src/trilha-backend/listaDeExercicios1/exercicio01/Pessoa.java
@@ -1,33 +1,42 @@
 package listaDeExercicios1.exercicio01;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * @author José Orlando R. Vilar
+ */
 public class Pessoa {
     private String nome;
     private Date dataNascimento;
     private Double altura;
+    private Integer idade;
+    private SimpleDateFormat formato;
 
-    public Pessoa(String nome, Date dataNascimento, Double altura) {
+    public Pessoa(String nome, String dataNascimento, Double altura) throws ParseException {
+        this.formato = new SimpleDateFormat("dd/MM/yyyy");
         this.nome = nome;
-        this.dataNascimento = dataNascimento;
+        this.dataNascimento = formato.parse(dataNascimento);
         this.altura = altura;
+        this.idade = calcularIdade();
     }
 
     public void imprimirDados() {
-        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
         System.out.println("\nDados da Pessoa:");
         System.out.println("Nome: " + this.nome);
-        System.out.println(this.dataNascimento.getTime());
-        System.out.println("Data de Nascimento: " + formato.format(this.dataNascimento.getTime()));
+        System.out.println("Data de Nascimento: " + this.formato.format(this.dataNascimento.getTime()));
         System.out.println("Altura: " + this.altura);
+        System.out.println("Idade: " + this.idade);
     }
 
-    public void calcularEImprimirIdade() {
+    public Integer calcularIdade() {
         Date dataAtual = new Date();
         int idade = 0;
 
-        if(dataAtual.getMonth() >= this.dataNascimento.getMonth()) {
+        if(dataAtual.getMonth() > this.dataNascimento.getMonth()) {
+            idade = dataAtual.getYear() - this.dataNascimento.getYear();
+        }else if(dataAtual.getMonth() == this.dataNascimento.getMonth()) {
             if(dataAtual.getDate() >= this.dataNascimento.getDate()) {
                 idade = dataAtual.getYear() - this.dataNascimento.getYear();
             }else {
@@ -37,7 +46,7 @@ public class Pessoa {
             idade = dataAtual.getYear() - this.dataNascimento.getYear() - 1;
         }
 
-        System.out.println("A idade de " + this.nome + " é: " + idade + " anos.");
+        return idade;
     }
 
     public String getNome() {
@@ -52,6 +61,10 @@ public class Pessoa {
         return altura;
     }
 
+    public Integer getIdade() {
+        return idade;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -62,5 +75,9 @@ public class Pessoa {
 
     public void setAltura(Double altura) {
         this.altura = altura;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
     }
 }
