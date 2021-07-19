73a6e2d54db5f61fc586ba2f4f150087a9cb7c1d
Orlando <muckyo@gmail.com>
2021-07-19 15:13:09 -0300

    Adicionando códigos da aula sobre Exceções em JAva

--- src/trilha-backend/javaPilha/Fluxo.java
+++ src/trilha-backend/javaPilha/Fluxo.java
@@ -1,13 +1,14 @@
 package javaPilha;
 
 public class Fluxo {
-
     public static void main(String[] args) {
         System.out.println("Ini do main");
         try {
             metodo1();
-        }catch(ArithmeticException | NullPointerException ex) {
-            System.out.println("Erro: " + ex.getMessage());
+        } catch (ArithmeticException | NullPointerException ex) {
+            String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+            ex.printStackTrace();
         }
         System.out.println("Fim do main");
     }
@@ -19,13 +20,8 @@ public class Fluxo {
     }
 
     private static void metodo2() {
-        System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            //i = i / 0;
-            Conta c = null;
-            c.depositar();
-        }
-        System.out.println("Fim do metodo2");
+        System.out.println("Ini do metodo 2");
+        metodo2();
+        System.out.println("Fim do metodo 2");
     }
 }
