75a43c0df07ddf9aff552834ee57725d8013df4d
Orlando <muckyo@gmail.com>
2021-07-26 14:59:34 -0300

    Adicionando códigos da aula sobre Java.io

---
+++ src/trilha-backend/javaIO/br/com/alura/java/io/teste/TesteLeituraEscrita.java
@@ -0,0 +1,78 @@
+package javaIO.br.com.alura.java.io.teste;
+
+import java.io.*;
+
+public class TesteLeituraEscrita {
+
+    public static void main(String[] args) throws IOException {
+        //lerArquivo();
+        //escreverArquivo();
+        //lerEscreverArquivo();
+        criarArquivoFileWriter();
+    }
+
+    public static void lerArquivo() throws IOException {
+        // Fluxo de Entrada com arquivo com Padrão de Projeto Decorator
+        FileInputStream fis = new FileInputStream("lorem.txt"); // recebendo o
+        // arquivo e criando o fluxo concreto, mas ainda binário
+        InputStreamReader isr = new InputStreamReader(fis); // melhorando os dados binários
+        // para caracteres
+        BufferedReader br = new BufferedReader(isr);// trabalhando com os caracteres linha por linha
+        String linha = br.readLine();
+
+        while(linha != null) {
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+
+        br.close();
+    }
+
+    public static void escreverArquivo() throws IOException {
+        OutputStream fos = new FileOutputStream("lorem2.txt");
+        Writer osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ");
+        bw.newLine();
+        //bw.newLine();
+        bw.write("José Orlando Ramos Vilar");
+
+        bw.close();
+    }
+
+    public static void lerEscreverArquivo() throws IOException {
+        InputStream fis = System.in;
+        //InputStream fis = new FileInputStream("lorem.txt");
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        OutputStream fos = System.out;
+        //OutputStream fos = new FileOutputStream("lorem2.txt");
+        Writer osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        String linha = br.readLine();
+
+        while (linha != null && !linha.isEmpty()) {
+            bw.write(linha);
+            bw.newLine();
+            bw.flush();
+            linha = br.readLine();
+        }
+
+        br.close();
+        bw.close();
+    }
+
+    public static void criarArquivoFileWriter() throws IOException {
+        FileWriter fw = new FileWriter("lorem3.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("José Orlando Ramos Vilar");
+        bw.newLine();
+        bw.newLine();
+        bw.write("Estagiário");
+
+        bw.close();
+    }
+}
