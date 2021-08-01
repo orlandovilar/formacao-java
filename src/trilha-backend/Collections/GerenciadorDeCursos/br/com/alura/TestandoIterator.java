bb521e86d68fa229857c261e59d539b36d306939
orlandovilar <muckyo@gmail.com>
2021-08-01 18:09:02 -0300

    Adicionando códigos das aulas sobre Java Collection e Iterator

---
+++ src/trilha-backend/Collections/GerenciadorDeCursos/br/com/alura/TestandoIterator.java
@@ -0,0 +1,19 @@
+package Collections.GerenciadorDeCursos.br.com.alura;
+
+import java.util.*;
+
+public class TestandoIterator {
+    public static void main(String[] args) {
+
+        List<String> letras = new LinkedList<>();
+        letras.add("A");
+        letras.add("B");
+        letras.add("C");
+
+        Iterator<String> it = letras.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
