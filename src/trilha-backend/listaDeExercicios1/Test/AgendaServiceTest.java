bd08d259c777633b07c9b8803a92d04bd21c0e1f
Orlando <muckyo@gmail.com>
2021-08-05 14:03:56 -0300

    Adicionando Testes a Lista de Exercícios - Peer Review

---
+++ src/trilha-backend/listaDeExercicios1/Test/AgendaServiceTest.java
@@ -0,0 +1,32 @@
+package listaDeExercicios1.Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AgendaServiceTest {
+
+    @Test
+    void adicionarPessoa() {
+    }
+
+    @Test
+    void testAdicionarPessoa() {
+    }
+
+    @Test
+    void removerPessoa() {
+    }
+
+    @Test
+    void buscarPessoa() {
+    }
+
+    @Test
+    void imprimirPessoa() {
+    }
+
+    @Test
+    void imprimirAgenda() {
+    }
+}
